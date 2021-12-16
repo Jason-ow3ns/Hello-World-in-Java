5722aa239329609b1326be1731f665fb86696d78
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-16 12:49:09 -0800

    Add files via upload

---
+++ printingIntVariable.java
@@ -0,0 +1,8 @@
+public class printingIntVariable {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+
+}
---
+++ sumOfVariables.java
@@ -0,0 +1,13 @@
+public class sumOfVariables {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+        int mySecondNumber = (10+12) - 5;
+        int myThirdNumber = 7*2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+    }
+}
+
