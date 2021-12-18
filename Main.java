718505d78533efee6eae820072c8c99229dbe732
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-17 16:45:05 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myCharValue = 'd';
+        char myUnicodeClockValue = '\u23F0';
+        System.out.println(myCharValue);
+        System.out.println(myUnicodeClockValue);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+        System.out.println(myTrueBoolean);
+        System.out.println(myFalseBoolean);
+
+
+    }
+}
