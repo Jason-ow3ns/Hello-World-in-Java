cb1b08893cacd87a6c9a9ecdac7d2ded8c90ad0a
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-17 16:20:45 -0800

    Add files via upload

---
+++ primiteTypeMinandMaxTwo.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class primiteTypeMinandMax {
+
+    public static void main(String[] args) {
+
+        float myFloatMax = Float.MAX_VALUE;
+        float myFloatMin = Float.MIN_VALUE;
+        System.out.println("Float Max = " + myFloatMax);
+        System.out.println("Float Min = " + myFloatMin);
+
+        double myDoubleMax = Double.MAX_VALUE;
+        double myDoubleMin = Double.MIN_VALUE;
+        System.out.println("Double Max = " + myDoubleMax);
+        System.out.println("Double Min = " + myDoubleMin);
+
+    }
+
+    
+    
+
+}
