eaae719a70501fbe1071f09bccb810467d4c5a47
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-18 17:10:05 -0800

    Add files via upload

---
+++ operatorAbbreviations.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class operatorAbbreviations {
+
+    public static void main(String[] args) {
+
+        // +1 abbreviation
+        int result = 1;
+        result++;
+        System.out.println(" 1 + 1 = " + result);
+
+        // -1 abbreviation
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        // +x abbreviation
+        result += 3;
+        System.out.println("1 + 3 = " + result);
+
+        // -x abbreviation
+        result -= 2;
+        System.out.println("4 - 2 = " + result);
+
+        // *x abbreviation
+        result *= 4;
+        System.out.println("2 * 4 = " + result);
+
+        // /x abbreviation
+        result /= 2;
+        System.out.println("8 / 2 = " + result);
+
+        // %x abbreviation
+        result %= 3;
+        System.out.println("4 % 2 = " + result);
+
+    }
+}
