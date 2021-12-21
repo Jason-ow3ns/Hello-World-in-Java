7d3bd55889e552d22b4e09f8ef4d52dac9ab5ad2
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-21 15:35:56 -0800

    Add files via upload

---
+++ keywordsAndExpressions.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class keywordsAndExpressions {
+
+    public static void main(String[] args) {
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You Got The High Score!");
+        }
+            score = 0;
+            // Expressions are score = 100, score > 99, "You Got The High Score!", score = 0
+
+    }
+}
