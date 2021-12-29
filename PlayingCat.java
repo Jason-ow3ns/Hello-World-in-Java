768fbedbfbc7ad42bf23989f77a408a4bbbfa231
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2021-12-29 14:27:30 -0800

    Add files via upload

---
+++ PlayingCat.java
@@ -0,0 +1,13 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (!summer && (temperature >= 25 && temperature <= 35)) {
+            return true;
+        } else return summer && (temperature >= 25 && temperature <= 45);
+
+
+    }
+}
