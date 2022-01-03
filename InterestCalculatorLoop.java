3e4437d4408494ab6422d19ec5b35cd699f80983
Jason-ow3ns <67971728+Jason-ow3ns@users.noreply.github.com>
2022-01-03 14:45:33 -0800

    Add files via upload

---
+++ InterestCalculatorLoop.java
@@ -0,0 +1,18 @@
+public class InterestCalculatorLoop {
+    public static void main(String[] args) {
+
+        for (int i=2; i<9; i++) {
+            System.out.println(calculateInterest(10000, i));
+        }
+
+        for (int i=8; i>1; i--) {
+            System.out.println(calculateInterest(10000, i));
+        }
+
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+
+}
